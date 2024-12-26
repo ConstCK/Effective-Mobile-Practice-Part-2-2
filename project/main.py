08e56844da7b66524fe5e656b0424667c5928074
Constantin <Constantin_CK@outlook.com>
2024-12-26 22:28:41 +0300

    Add parser and data filter

--- project/main.py
+++ project/main.py
@@ -1,13 +1,18 @@
 import asyncio
 
+from services.parser import Parser
 from services.services import create_tables
-'https://spimex.com/files/31763/'
+
+my_service = Parser('https://spimex.com/markets/oil_products/trades/results/')
 
 
 async def main():
-    # запуск функции с созданием всех таблиц в БД
-    # await create_tables()
-    pass
+    #     # запуск функции с созданием всех таблиц в БД
+    #     # await create_tables()
+    await my_service.get_xls_urls(2024)
+    await my_service.get_xls_data()
+    result = await my_service.get_buffer_data()
+    print(result)
 
 
 if __name__ == '__main__':
@@ -15,3 +20,5 @@ if __name__ == '__main__':
         asyncio.run(main())
     except Exception:
         print('There are some problems running application...exiting program...')
+    # my_service.get_xls_urls(2024)
+    # my_service.get_xls_data()
