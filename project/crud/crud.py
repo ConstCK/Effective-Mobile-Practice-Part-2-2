0b07bd2e5ea837fdb9e21a5c10004d893dbcdaca
Constantin <Constantin_CK@outlook.com>
2024-12-28 18:57:20 +0300

    Refactor crud

--- project/crud/crud.py
+++ project/crud/crud.py
@@ -10,26 +10,25 @@ class TradingExchange:
 
     async def add_data(self, data: list[dict[str, str | int]]) -> None:
         print('Starting data transfering from local storage to DB...')
-        for i in data:
-            stmt = insert(TradingResult).values(
-                exchange_product_id=i['exchange_product_id'],
-                exchange_product_name=i['exchange_product_name'],
-                oil_id=i['oil_id'],
-                delivery_basis_id=i['delivery_basis_id'],
-                delivery_basis_name=i['delivery_basis_name'],
-                delivery_type_id=i['delivery_type_id'],
-                volume=i['volume'],
-                total=int(i['total']),
-                count=i['count'],
-                date=i['date'],
-            )
-            try:
-                await self.session.execute(stmt)
-                await self.session.commit()
-            except Exception as error:
-                print('Error adding data to DB...', error)
-                continue
-            finally:
-                await self.session.close()
+        async with self.session as s:
+            for i in data:
+                stmt = insert(TradingResult).values(
+                    exchange_product_id=i['exchange_product_id'],
+                    exchange_product_name=i['exchange_product_name'],
+                    oil_id=i['oil_id'],
+                    delivery_basis_id=i['delivery_basis_id'],
+                    delivery_basis_name=i['delivery_basis_name'],
+                    delivery_type_id=i['delivery_type_id'],
+                    volume=i['volume'],
+                    total=int(i['total']),
+                    count=i['count'],
+                    date=i['date'],
+                )
+                try:
+                    await s.execute(stmt)
+                    await s.commit()
+                except Exception as error:
+                    print('Error adding data to DB...', error)
+                    continue
 
         print('All data is successfully added to DB...')
