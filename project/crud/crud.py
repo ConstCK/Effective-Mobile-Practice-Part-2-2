3e7657a64b0784b6ff6f8f6db8cff48fc4191c78
Constantin <Constantin_CK@outlook.com>
2024-12-29 18:48:01 +0300

    Add some crud methods and refactor some code

--- project/crud/crud.py
+++ project/crud/crud.py
@@ -1,7 +1,11 @@
-from sqlalchemy import insert
+import datetime
+from sqlalchemy import select, update, insert
+from sqlalchemy.dialects.postgresql import insert as insert_with_conflict
+
 
 from db.db import async_session
 from models.models import TradingResult
+from services.initial_tasks import create_tables, delete_tables
 
 
 class TradingExchange:
@@ -9,6 +13,9 @@ class TradingExchange:
         self.session = async_session()
 
     async def add_data(self, data: list[dict[str, str | int]]) -> None:
+        """Метод для создания таблицы с данными с торговой биржи с нуля"""
+        await delete_tables()
+        await create_tables()
         print('Starting data transfering from local storage to DB...')
         async with self.session as s:
             for i in data:
@@ -31,4 +38,64 @@ class TradingExchange:
                     print('Error adding data to DB...', error)
                     continue
 
+        print('All data is successfully added to DB...')
+
+
+# Альтернативный вариант добавления данных в БД
+
+    async def add_all_data(self, data: list[dict[str, str | int]]) -> None:
+        """Метод для создания таблицы с данными с торговой биржи с нуля"""
+        await delete_tables()
+        await create_tables()
+
+        print('Starting data transfering from local storage to DB...')
+        async with self.session as s:
+            entries = [TradingResult(**i) for i in data]
+            try:
+                s.add_all(entries)
+                await s.commit()
+            except Exception as error:
+                print('Error adding data to DB...', error)
+
+        print('All data is successfully added to DB...')
+
+# Альтернативный вариант добавления данных в БД с обновлением существующих записей
+    async def add_or_update_data(self, data: list[dict[str, str | int]]) -> None:
+        """Метод для создания/обновления данных в таблице с результатами торгов"""
+
+        print('Starting data transfering from local storage to DB...')
+        async with self.session as s:
+
+            for i in data:
+                stmt = select(TradingResult).where(
+                    (TradingResult.exchange_product_id ==
+                     i['exchange_product_id'])
+                    & (TradingResult.date == i['date'])
+                )
+                obj = await s.execute(stmt)
+                result = obj.scalar()
+
+                if not result:
+                    s.add(TradingResult(**i))
+                    await s.commit()
+                else:
+                    stmt = update(TradingResult).where(
+                        TradingResult.exchange_product_id == i['exchange_product_id']).values()
+                    await s.execute(stmt)
+                    await s.commit()
+
+        print('All data is successfully added to DB...')
+
+# Еще один альтернативный вариант добавления данных в БД с обновлением существующих записей!
+    async def create_or_update_data(self, data: list[dict[str, str | int]]) -> None:
+        """Метод для создания/обновления данных в таблице с результатами торгов"""
+
+        print('Starting data transfering from local storage to DB...')
+        async with self.session as s:
+            for i in data:
+                stmt = insert_with_conflict(TradingResult).values(
+                    **i).on_conflict_do_update(index_elements=['exchange_product_id', 'date'],
+                                               set_=dict(updated_on=datetime.datetime.now()))
+                await s.execute(stmt)
+                await s.commit()
         print('All data is successfully added to DB...')
--- project/main.py
+++ project/main.py
@@ -2,22 +2,18 @@ import asyncio
 
 from config import MAIN_URL
 from crud.crud import TradingExchange
-from services.initial_tasks import create_tables, delete_tables
 from services.parser import Parser
-from services.services import DataService
+
 
 my_parser = Parser(MAIN_URL)
 db_service = TradingExchange()
 
 
 async def main():
-    await delete_tables()
-    await create_tables()
-
     await my_parser.get_xls_urls(2023)
     await my_parser.get_xls_data()
     result = await my_parser.get_all_data()
-    await db_service.add_data(result)
+    await db_service.create_or_update_data(result)
 
 
 if __name__ == '__main__':
